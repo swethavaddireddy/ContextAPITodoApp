1381b3cae0dc660bb1f0a1973b68856c29acf705
swethavaddireddy <swethavaddireddy92@gmail.com>
2024-03-20 15:30:06 +0530

    to build ToDo app with context API

--- src/App.js
+++ src/App.js
@@ -5,17 +5,6 @@ import TodoList from "./components/TodoList/TodoList";
 
 function App() {
 
-
-  // const deleteTodoHandler = (todoItem) => {
-  //   todoItem.completed ? setCompletedTodos(completedTodos.filter((todo) => todo.id !== todoItem.id)) :
-  //     setTodoList(todoList.filter(todo => todo.id !== todoItem.id))
-  // }
-
-  // const completedTodo = (completeTodo) => {
-  //   setCompletedTodos([...completedTodos, completeTodo])
-  //   setTodoList(todoList.filter(todo => todo.id !== completeTodo.id))
-  // }
-
   return (
     <div className="app">
       <div className="container">
